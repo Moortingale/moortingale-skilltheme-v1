844b33f5ea820557a6eb178b413de5d103f158a4
Moortingale <last_sabre78@hotmail.com>
2020-07-02 19:53:05 +0800

    modifty theme folder

--- skillate/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div id="sidebar" role="complementary">
-    <aside class="widget-area">
-        <?php 
-	        if ( is_active_sidebar( 'sidebar' ) ) {
-	        	dynamic_sidebar('sidebar');
-	        }
-         ?>
-    </aside>
-</div> <!-- #sidebar -->
